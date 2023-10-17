d3aafdb6281e43f14b0fbf68e660f13483af5480
MoulOut <mmoulinlima@outlook.com>
2023-10-17 19:24:07 -0300

    add environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { server } from './Server/Server';
 
 const startServer = () => {
-  server.listen(3333, () => console.log('App rodando na porta 3333'));
+  server.listen(process.env.PORT || 3333, () =>{
+    console.log(`App rodando na porta ${process.env.PORT}`);
+    
+  });
 };
 
 startServer();
