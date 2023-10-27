2bfc1b8c8e18960d0285d0f54f6b91a0a5a1f142
MoulOut <mmoulinlima@outlook.com>
2023-10-26 22:02:50 -0300

    fix for deploy

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,17 @@ import { server } from './Server/Server';
 import { Knex } from './Server/database/knex';
 
 const startServer = () => {
-  server.listen(process.env.PORT || 3333, () =>{
+  server.listen(process.env.PORT || 3333, () => {
     console.log(`App rodando na porta ${process.env.PORT}`);
-    
   });
 };
 
 if (process.env.IS_LOCALHOST !== 'true') {
   console.log('Rodando migrations');
-
-  Knex.migrate.latest().then(() => {
-    Knex.seed
-      .run()
-      .then(() => startServer())
-      .catch(console.log);
-  });
+  Knex.migrate
+    .latest()
+    .then(() => startServer())
+    .catch(console.log);
 } else {
   startServer();
 }
