e433d62786a3069d809111b3e05ac570eed13e86
MoulOut <mmoulinlima@outlook.com>
2023-10-26 21:37:01 -0300

    add CORS

--- src/Server/Server.ts
+++ src/Server/Server.ts
@@ -1,10 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 import './shared/services/YupTranslation';
 import { router } from './routes';
 
 const server = express();
 
+server.use(
+  cors({
+    origin: process.env.ENABLED_CORS?.split(';') || [],
+  })
+);
 server.use(express.json());
 server.use(router);
 
